6645ad9c8220c139bbeabaa081db38ffd97cb5d4
Yannick Mauray <yannick.mauray@gmail.com>
2015-04-03 16:10:40 +0200

    Modifications pour Tomcat7

--- modules/tomcat/manifests/init.pp
+++ modules/tomcat/manifests/init.pp
@@ -1,10 +1,11 @@
 class tomcat {
 
-	package { "tomcat7" : ensure => latest }
-	package { "tomcat7-admin" : ensure => latest }
-	package { "tomcat7-user" : ensure => latest }
+	package { "tomcat7" : ensure => "latest" }
+	package { "tomcat7-admin" : ensure => "latest" }
+	package { "tomcat7-user" : ensure => "latest" }
 
 	service { "tomcat7" :
+		require => Package["tomcat7", "tomcat7-admin", "tomcat7-user"],
 		ensure => running,
 		enable => true
 	}
